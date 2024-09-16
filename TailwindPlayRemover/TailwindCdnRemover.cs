37ba55be10362342fe73b61fb161dcced39fef12
Sebastian Tramontana <Sebastian.Tramontana@endava.com>
2024-09-16 10:37:21 -0300

    Create remover

---
+++ TailwindPlayRemover/TailwindCdnRemover.cs
@@ -0,0 +1,36 @@
+﻿using Cake.Core.Annotations;
+using HtmlAgilityPack;
+
+[assembly: CakeNamespaceImport("HtmlAgilityPack")]
+namespace TailwindPlayRemover;
+
+public static class TailwindCdnRemover
+{
+    public static void RemoveCdn(string htmlFileFullPath)
+    {
+        var htmlString = File.ReadAllText(htmlFileFullPath);
+
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(htmlString);
+
+        var head = htmlDoc.DocumentNode.SelectSingleNode("//head");
+
+        if (head is null)
+        {
+            Console.WriteLine("Head node not found!");
+            return;
+        }
+
+        var targetSrc = "https://cdn.tailwindcss.com";
+        var tailwindCdnNode = head!.SelectSingleNode($"//script[@src='{targetSrc}']");
+
+        if (tailwindCdnNode is null)
+        {
+            Console.WriteLine("Tailwind's cdn node not found!");
+            return;
+        }
+
+        head.RemoveChild(tailwindCdnNode);
+        htmlDoc.Save(htmlFileFullPath);
+    }
+}
---
+++ TailwindPlayRemover/TailwindCdnRemoverCakeExtension.cs
@@ -0,0 +1,12 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+
+namespace TailwindPlayRemover;
+
+public static class TailwindCdnRemoverCakeExtension
+{
+    [CakeMethodAlias]
+    public static void RemoveTailwindCdn(this ICakeContext context, FilePath htmlFile)
+        => TailwindCdnRemover.RemoveCdn(htmlFile.FullPath);
+}
